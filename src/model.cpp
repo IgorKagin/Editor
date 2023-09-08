3cdefdb38b00d245d45a02bae59015717e21ee84
IgorKagin <60917726+IgorKagin@users.noreply.github.com>
2023-09-08 15:39:38 +0300

    Create model.cpp

---
+++ src/model.cpp
@@ -0,0 +1,25 @@
+#include "model.h"
+
+using namespace Model;
+
+void DocsCore::createDoc()
+{
+  std::cout << "createDoc()" << std::endl;
+}
+void DocsCore::exportDoc()
+{
+  std::cout << "exportDoc()" << std::endl;
+}
+void DocsCore::importDoc()
+{
+  std::cout << "importDoc()" << std::endl;
+}
+
+void WidgetCore::createWidget()
+{
+  std::cout << "createWidget()" << std::endl;
+}
+void WidgetCore::deleteWidet()
+{
+  std::cout << "deleteWidet()" << std::endl;
+}
